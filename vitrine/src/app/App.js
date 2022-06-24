82678807b8fee316b59ee9666d6aa28e660df8e4
Louis Berthaut <louis.berthaut@gmx.com>
2022-06-24 18:02:49 +0200

    add structure

---
+++ vitrine/src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+
+
+      </BrowserRouter>
+    )
+  }
+}
